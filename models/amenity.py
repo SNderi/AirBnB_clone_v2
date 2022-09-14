59fa3bc7dc84f35249f350a63e4291021261d285
SNderi <sharonscarlet8@gmail.com>
2022-09-14 19:58:47 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
-""" Amenity Module for HBNB project """
-import models
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models import storage_type
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """ Represent a the Amenities for a place
-    
-    Inherits the BaseModel and Base(from sqlalchemy) and links to the mysql table amenities
-    Attributes:
-      __tablename__(str): name of the MySQL table
-      name(sqlalchemy String): name of the City
-    """
-    if models.storage_type == "db":
-        __tablename__ = "amenities"
+    '''amenity class'''
+    __tablename__ = 'amenities'
+    if storage_type == 'db':
         name = Column(String(128), nullable=False)
     else:
-        def __init__(self, name = ""):
-            """ if storage is not db (i.e. FileStorage) instantiate the values """
-            self.name = name
-            super().__init__()
+        name = ""
