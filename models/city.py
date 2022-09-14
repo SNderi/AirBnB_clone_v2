76cbc0c4b27ebbc6487098fa2b9e6d0278f1c6bb
SNderi <sharonscarlet8@gmail.com>
2022-09-14 20:00:48 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-import models
 from models.base_model import BaseModel, Base
+from models import storage_type
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """ The city class
-    Attributes:
-        state_id
-        name
-    """
-    if models.storage_type == "db":
-        __tablename__ = 'cities'
+    """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
+    if storage_type == 'db':
         name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-        places = relationship("Place", backref="cities")
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', backref='cities',
+                              cascade='all, delete, delete-orphan')
     else:
-        def __init__(self, state_id = "", name = ""):
-            """ if storage type is FileStorage instantiate the values """
-            self.state_id = state_id
-            self.name = name
-            super().__init__()
+        name = ''
+        state_id = ''
